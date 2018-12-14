12b1e594dc54aedcad19552db7028982ed0e1aab
Qiwen Wang <qiwen.wang@163.com>
2018-12-14 03:49:34 +0000

    tutorial

--- cleverhans_tutorials/build_models.py
+++ cleverhans_tutorials/build_models.py
@@ -1,6 +1,6 @@
-python3 mnist_tutorial_tf.py 
-python3 mnist_tutorial_tf.py  --preprocess gaussian
-python3 mnist_tutorial_tf.py  --preprocess sqeeze
+#python3 mnist_tutorial_tf.py 
+#python3 mnist_tutorial_tf.py  --preprocess gaussian
+#python3 mnist_tutorial_tf.py  --preprocess sqeeze
 python3 mnist_blackbox.py
 python3 mnist_blackbox.py --preprocess gaussian
 python3 mnist_blackbox.py --preprocess sqeeze
